0fb24afbd77cc1861f97c85295c46eb172df95e1
Adam Gray <a.gray.4797@gmail.com>
2021-05-31 16:22:02 +0100

    used guards to combine upper/lowercase letter conversions to ints

--- caesar.hs
+++ caesar.hs
@@ -1,15 +1,14 @@
 import Data.Char
 
-lower_let_to_int c = ord c - ord 'a'
+let_to_int c | isLower c = ord c - ord 'a'
+             | isUpper c = ord c - ord 'A'
 
 int_to_lower_let n = chr (ord 'a' + n)
 
-upper_let_to_int c = ord c - ord 'A'
-
 int_to_upper_let n = chr (ord 'A' + n)
 
-shift n c | isLower c = int_to_lower_let ((lower_let_to_int c + n) `mod` 26)
-          | isUpper c = int_to_upper_let ((upper_let_to_int c + n) `mod` 26)
+shift n c | isLower c = int_to_lower_let ((let_to_int c + n) `mod` 26)
+          | isUpper c = int_to_upper_let ((let_to_int c + n) `mod` 26)
           | otherwise = c
 
 encode n xs = [shift n x | x <- xs]
