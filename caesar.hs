38337f62ffb77348600fcc4d965a954f055ea291
Adam Gray <a.gray.4797@gmail.com>
2021-05-31 16:17:44 +0100

    caesar.hs now ciphers upper case letters as well

--- caesar.hs
+++ caesar.hs
@@ -1,12 +1,18 @@
 import Data.Char
 
-lettoint c = ord c - ord 'a'
+lower_let_to_int c = ord c - ord 'a'
 
-inttolet n = chr (ord 'a' + n)
+int_to_lower_let n = chr (ord 'a' + n)
 
-shift n c | isLower c = inttolet ((lettoint c + n) `mod` 26) | otherwise = c
+upper_let_to_int c = ord c - ord 'A'
+
+int_to_upper_let n = chr (ord 'A' + n)
+
+shift n c | isLower c = int_to_lower_let ((lower_let_to_int c + n) `mod` 26)
+          | isUpper c = int_to_upper_let ((upper_let_to_int c + n) `mod` 26)
+          | otherwise = c
 
 encode n xs = [shift n x | x <- xs]
 
 main = do
-  print $ encode 3 "haskell is fun"
+  print $ encode 3 "Haskell is fun"
