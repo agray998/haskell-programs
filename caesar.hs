a144349361c24c04c64ed51a48d65385f784e329
agray998 <a.gray.4797@gmail.com>
2021-06-04 21:47:55 +0100

    Added crack function to caesar.hs

--- caesar.hs
+++ caesar.hs
@@ -1,4 +1,10 @@
 import Data.Char
+import Data.Typeable
+count x xs = length [x' | x' <- xs, x == x']
+
+letters xs = length [x | x <- xs, typeOf x == typeOf 'a']
+
+positions x xs = [i | (x', i) <- zip xs [0..], x == x']
 
 let_to_int c | isLower c = ord c - ord 'a'
              | isUpper c = ord c - ord 'A'
@@ -13,5 +19,22 @@ shift n c | isLower c = int_to_lower_let ((let_to_int c + n) `mod` 26)
 
 encode n xs = [shift n x | x <- xs]
 
+freq_table = [8.1, 1.5, 2.8, 4.2, 12.7, 2.2, 2.0, 6.1, 7.0, 0.2, 0.8, 4.0, 2.4, 6.7, 7.5, 1.9, 0.1, 6.0, 6.3, 9.0, 2.8, 1.0, 2.4, 0.2, 2.0, 0.1]
+
+percent n m = (fromIntegral n / fromIntegral m) * 100
+
+freqs xs = [percent (count x xs) (letters xs) | x <- ['a'..'z']]
+
+chi_sqr os es = sum [((o - e) ^ 2) / e | (o, e) <- zip os es]
+
+rotate n xs = drop n xs ++ take n xs
+
+crack xs = encode (-factor) xs
+           where
+             factor = head (positions (minimum chi_tab) chi_tab)
+             chi_tab = [chi_sqr (rotate n (freqs xs)) freq_table | n <- [0..25]]
+
 main = do
-  print $ encode 3 "Haskell is fun"
+  let c_text = encode 3 "Haskell is fun"
+  print c_text
+  print $ crack c_text
