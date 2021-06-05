f2716ca31202c80b20810957325900efa05e1af8
Adam Gray <a.gray.4797@gmail.com>
2021-06-05 21:50:12 +0100

    altered fizzbuzz to use a loop rather than list comprehension

--- fizzbuzz.hs
+++ fizzbuzz.hs
@@ -4,5 +4,10 @@ fizzbuzz n | ((n `mod` 3 == 0) && (n `mod` 5 == 0)) = "FizzBuzz"
            | ((n `mod` 3 /= 0) && (n `mod` 5 == 0)) = "Buzz" 
            | otherwise = show n 
 
+loop i = do
+  print $ fizzbuzz i
+  let a = i + 1
+  if a < 100 then loop a else print "done"
 
-main = print ([fizzbuzz n | n <- [1..100]])
+main = do
+  loop 1
