a79c6e7bc30668745ea080f1fcdac052240579e4
Adam Gray <a.gray.4797@gmail.com>
2021-06-05 21:56:17 +0100

    loop in fizzbuzz can now specify upper and lower bound

--- fizzbuzz.hs
+++ fizzbuzz.hs
@@ -4,10 +4,10 @@ fizzbuzz n | ((n `mod` 3 == 0) && (n `mod` 5 == 0)) = "FizzBuzz"
            | ((n `mod` 3 /= 0) && (n `mod` 5 == 0)) = "Buzz" 
            | otherwise = show n 
 
-loop i = do
+loop i n = do
   print $ fizzbuzz i
   let a = i + 1
-  if a < 100 then loop a else print "done"
+  if a <= n then loop a n else print "done"
 
 main = do
-  loop 1
+  loop 1 100
